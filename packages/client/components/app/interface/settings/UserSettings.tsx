45a004e178aa11755397974f0156a36dbf223680
Bryan White <bryanchriswhite@proton.me>
2026-04-29 08:23:44 +0000

    chore(brand): revert upstream donate item and source-code URL changes
    Reverts two upstream regressions that were cherry-picked into staging
    but never landed on main. Brings the user-settings menu back into
    alignment with the debranding methodology.
    
    - Remove the Donate entry (introduced upstream by PR #573, cherry-picked
      into staging as 7a42c98b). It pointed to ko-fi.com/stoatchat, which
      is the upstream Stoat brand we are debranding away from. Our project
      is not donation-funded and has no equivalent destination.
    
    - Restore the Source Code link to work.episkopos.community/episkopos/community
      (introduced upstream by PR #622, cherry-picked into staging as 62ac91a4,
      which had pointed it at github.com/stoatchat). The community repo on our
      self-hosted GitLab is the hub for all Censer-related repos (web, Flutter,
      backend) and is the correct destination.
    
    Beads: stoat-frontend-cfc, stoat-frontend-w92
    Tracked under chat-psem (Upstream Sync Automation System).

--- packages/client/components/app/interface/settings/UserSettings.tsx
+++ packages/client/components/app/interface/settings/UserSettings.tsx
@@ -11,7 +11,6 @@ import { ColouredText, Column, Text, iconSize } from "@revolt/ui";
 import { Symbol } from "@revolt/ui/components/utils/Symbol";
 
 import MdAccountCircle from "@material-design-icons/svg/outlined/account_circle.svg?component-solid";
-import MdCoffee from "@material-design-icons/svg/outlined/coffee.svg?component-solid";
 import MdLanguage from "@material-design-icons/svg/outlined/language.svg?component-solid";
 import MdLogout from "@material-design-icons/svg/outlined/logout.svg?component-solid";
 import MdMemory from "@material-design-icons/svg/outlined/memory.svg?component-solid";
@@ -274,7 +273,7 @@ const Config: SettingsConfiguration<{ server: Server }> = {
             //   title: t("app.special.modals.changelogs.title"),
             // },
             {
-              href: "https://github.com/stoatchat",
+              href: "https://work.episkopos.community/episkopos/community",
               icon: <MdMemory {...iconSize(20)} />,
               title: <Trans>Source Code</Trans>,
             },
@@ -282,11 +281,6 @@ const Config: SettingsConfiguration<{ server: Server }> = {
               id: "advanced",
               icon: <MdScience {...iconSize(20)} />,
               title: <Trans>Advanced</Trans>,
-            },
-            {
-              href: "https://ko-fi.com/stoatchat",
-              icon: <MdCoffee {...iconSize(20)} />,
-              title: <Trans>Donate</Trans>,
             },
             {
               id: "logout",
